1571f26c1fdb1e003dc16b604444a67acfa7ab9d
Strannik93 <sergevsc@mail.ru>
2023-08-07 21:14:20 +0600

    bus ticket purchase app

---
+++ HomeWork4/HomeWork4/src/Models/Carrier.java
@@ -0,0 +1,24 @@
+package Models;
+
+/**
+ * Модель перевозчика
+ */
+public class Carrier {
+    private int id;
+    private long cardNumber;
+    private int zones;
+
+    public Carrier(int id, long cardNumber) {
+        this.id = id;
+        this.cardNumber = cardNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public long getCardNumber() {
+        return cardNumber;
+    }
+
+}
